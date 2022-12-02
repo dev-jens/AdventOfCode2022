2e6aa6353f979252100383e7b78aacc695441da6
dev-jens <baetenjens07@gmail.com>
2022-12-02 12:53:07 +0100

    day1

--- day01/part1.py
+++ day01/part1.py
@@ -1,10 +1,7 @@
-def main():
+
+def part1(lines):
     caloriesList= []
     sum = 0
-
-    with open("input.txt", 'r') as input:
-        lines = input.readlines()
-
     for line in lines:
         if line.strip() == "":
             caloriesList.append(sum)
@@ -14,7 +11,19 @@ def main():
             
     caloriesList.append(sum)
 
-    print(max(caloriesList))
+    print(f"Solution Part1 = {max(caloriesList)}")
+    return caloriesList
+
+def part2(caloriesList):
+    caloriesList.sort()
+    print(f"Solution Part2 = {sum(caloriesList[-3:])}")
+
+def main():
+    with open("input.txt", 'r') as input:
+        lines = input.readlines()
+
+    caloriesList = part1(lines)
+    part2(caloriesList)
 
 if __name__ == '__main__':
     main()
