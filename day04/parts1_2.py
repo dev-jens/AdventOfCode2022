5d1e46afa3c3b77be51f1515cfc97dceb188a966
dev-jens <baetenjens07@gmail.com>
2022-12-04 13:49:37 +0100

    day4

--- day04/parts1_2.py
+++ day04/parts1_2.py
@@ -14,17 +14,15 @@ def main():
         e1, e2 = line.split(',')
         s1Min, s1Max = e1.split('-')
         s2Min, s2Max = e2.split('-')
-        elv1 = []
-        elv2 = []
-        for i in range(int(s1Min), int(s1Max) + 1):
-            elv1.append(i)
-        for i in range(int(s2Min), int(s2Max) + 1):
-            elv2.append(i)
-   
+        elv1 = [int(x) for x in range(int(s1Min), int(s1Max) + 1)]
+        elv2 = [int(x) for x in range(int(s2Min), int(s2Max) + 1)]
+        # Part 1
         if (elv1[0] >= elv2[0] and elv1[-1] <= elv2[-1]) or (elv2[0] >= elv1[0] and elv2[-1] <= elv1[-1]):
             pairsP1 += 1
+        # Part 2
         if (elv1[0] >= elv2[0] and elv1[0] <= elv2[-1]) or (elv2[0] >= elv1[0] and elv2[0] <= elv1[-1]):
             pairsP2 += 1
+
     print("Part 1: " + str(pairsP1))
     print("Part 2: " + str(pairsP2))
    
